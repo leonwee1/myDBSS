662e3181dc422037130e1370ad9d573917b0e447
leonwee1 <HangLeong.Wee@gmail.com>
2025-07-14 06:24:46 +0000

    Updated

--- app_DBSS.py
+++ app_DBSS.py
@@ -7,12 +7,6 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST"])
-def main():
-    q = request.form.get("q")
-    # db
-    return(render_template("main.html"))
-
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
